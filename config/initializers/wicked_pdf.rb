dcdd624526a48240fe1d65ea29a74807df780ec9
lwong-ux <wong.instruments@gmail.com>
2021-08-20 11:34:57 -0500

    wicked-pdf para heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -15,7 +15,7 @@ WickedPdf.config = {
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
   # Descomentar la siguiente línea para la RaspberryPi:
-  exe_path: '/usr/bin/wkhtmltopdf',
+  #exe_path: '/usr/bin/wkhtmltopdf',
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
